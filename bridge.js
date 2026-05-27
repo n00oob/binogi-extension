82f0d50e7801e6d596aaaff4a6399efc9b91ed80
nooob <developer.n00ob@gmail.com>
2026-05-27 19:15:45 +0200

    Update bridge.js

--- bridge.js
+++ bridge.js
@@ -1,11 +1,9 @@
-// Listen for the "READY" signal from content.js
 window.addEventListener("message", (event) => {
   if (event.data && event.data.type === "CONTENT_SCRIPT_READY") {
     sendModeToPage();
   }
 });
 
-// Function to send current mode
 function sendModeToPage() {
   chrome.storage.local.get(['quizMode'], (result) => {
     const mode = result.quizMode || 'highlight';
@@ -13,12 +11,10 @@ function sendModeToPage() {
   });
 }
 
-// Also send mode if the user changes it in the popup
 chrome.runtime.onMessage.addListener((request) => {
   if (request.type === "UPDATE_MODE") {
     window.postMessage({ type: "FROM_EXTENSION", mode: request.mode }, "*");
   }
 });
 
-// Initial attempt just in case
 sendModeToPage();
